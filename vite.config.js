43cf98dc13bcb67c4249270c70727889d770e277
Decsika Helen Talluri <helendecsika5@gmail.com>
2026-03-12 16:38:54 +0530

    Update Vite server and preview configuration

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,     // dev port, optional in Docker
+    port: 3000,          // optional, keep your container port
+    host: true,          // allows external access (all hosts)
   },
   preview: {
-    port: 4173,     // matches container targetPort
-    host: true,     // allow external access (K8s pod)
-    allowedHosts: ['a9d24332604574c5489d8edd7d6a1bed-709405611.ap-south-1.elb.amazonaws.com']
+    host: '0.0.0.0',     // allow access from all network interfaces
+    port: 4173,          // match your deployment targetPort
+    allowedHosts: [
+      'a9d24332604574c5489d8edd7d6a1bed-709405611.ap-south-1.elb.amazonaws.com'
+    ]
   }
 })
