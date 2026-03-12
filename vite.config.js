ca704ef2db811fb618a261b080ed417764ccf9f4
Decsika Helen Talluri <helendecsika5@gmail.com>
2026-03-12 15:21:41 +0530

    Add allowedHosts configuration to Vite server

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000, // Optional: Keep the same port as CRA
+    allowedHosts: [
+    'a9d24332604574c5489d8edd7d6a1bed-709405611.ap-south-1.elb.amazonaws.com'
+      // or use '*' to allow all hosts (not recommended for production)
+    ]
     open: true // Optional: Automatically open the app in the browser
   }
 })
