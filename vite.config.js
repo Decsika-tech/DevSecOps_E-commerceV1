7c2ec1fab66c7ae4020e0a2b94ffaff5009cd8af
Decsika Helen Talluri <helendecsika5@gmail.com>
2026-03-17 14:00:18 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
-    allowedHosts: true
-  },
-  preview: {
-    allowedHosts: [
-      'localhost',
-      'a9d24332604574c5489d8edd7d6a1bed-709405611.ap-south-1.elb.amazonaws.com'
-    ]
+    port: 3000, // Optional: Keep the same port as CRA
+    open: true // Optional: Automatically open the app in the browser
   }
 })
