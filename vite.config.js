27f65b63f02dfaa6aa15e32d1d584df6ac82dba2
Decsika Helen Talluri <helendecsika5@gmail.com>
2026-03-17 14:46:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 3000, // Optional: Keep the same port as CRA
     open: true // Optional: Automatically open the app in the browser
   }
